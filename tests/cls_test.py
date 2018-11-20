05d76e021d18f5c1ea6f0a548008df81650fbac1
Taeber Rapczak <taeber@ufl.edu>
2018-11-20 15:46:04 -0500

    Add CLS form handling to FVP

--- tests/cls_test.py
+++ tests/cls_test.py
@@ -1,19 +1,25 @@
 import unittest
 
-from nacc.uds3 import packet as ivp_packet
-from nacc.uds3.ivp import builder
+from nacc.uds3 import clsform
+from nacc.uds3 import packet
+from nacc.uds3.fvp import forms as fvp_forms
+from nacc.uds3.ivp import forms as ivp_forms
 
 
 class TestCLS(unittest.TestCase):
 
     def test_cls_blank_not_added_to_ivp(self):
         """Don't add blank CLS form to IVP."""
-        packet = ivp_packet.Packet()
         record = make_blank_record()
         record['hispanic'] = '1'  # Subject is Hispanic/Latino.
 
-        builder.add_cls(record, packet)
-        self.assertEqual(len(packet), 0, "Expected packet to be empty")
+        ipacket = packet.Packet()
+        clsform.add_cls(record, ipacket, ivp_forms)
+        self.assertEqual(len(ipacket), 0, "Expected packet to be empty")
+
+        fpacket = packet.Packet()
+        clsform.add_cls(record, fpacket, fvp_forms)
+        self.assertEqual(len(fpacket), 0, "Expected packet to be empty")
 
     def test_cls_not_added_if_not_hispanic(self):
         """
@@ -21,59 +27,83 @@ class TestCLS(unittest.TestCase):
 
         https://www.alz.washington.edu/NONMEMBER/UDS/DOCS/VER3/CLS/CLS_en.pdf
         """
-        packet = ivp_packet.Packet()
         record = make_blank_record()
         record['hispanic'] = '0'  # Subject is not Hispanic/Latino.
 
-        builder.add_cls(record, packet)
-        self.assertEqual(len(packet), 0, "Expected packet to be empty")
+        ipacket = packet.Packet()
+        clsform.add_cls(record, ipacket, ivp_forms)
+        self.assertEqual(len(ipacket), 0, "Expected packet to be empty")
+
+        fpacket = packet.Packet()
+        clsform.add_cls(record, fpacket, fvp_forms)
+        self.assertEqual(len(fpacket), 0, "Expected packet to be empty")
 
     def test_cls_added_when_filled(self):
-        """Add filled CLS form to IVP."""
-        packet = ivp_packet.Packet()
+        """Add filled CLS form to IVP and FVP."""
         record = make_filled_record()
 
-        builder.add_cls(record, packet)
-        self.assertEqual(len(packet), 1, "Expected packet to have CLS")
+        ipacket = packet.Packet()
+        clsform.add_cls(record, ipacket, ivp_forms)
+        self.assertEqual(len(ipacket), 1, "Expected packet to have CLS")
+
+        fpacket = packet.Packet()
+        clsform.add_cls(record, fpacket, fvp_forms)
+        self.assertEqual(len(fpacket), 1, "Expected packet to have CLS")
 
     def test_partial_cls_raises_error(self):
         """Partially completed CLS should raise an exception."""
-        packet = ivp_packet.Packet()
         record = make_filled_record()
         record['eng_preferred_language'] = ' '  # Make form partially complete.
 
+        ipacket = packet.Packet()
         with self.assertRaises(Exception):
-            builder.add_cls(record, packet)
+            clsform.add_cls(record, ipacket, ivp_forms)
+
+        fpacket = packet.Packet()
+        with self.assertRaises(Exception):
+            clsform.add_cls(record, fpacket, fvp_forms)
 
     def test_cls_proficiency_must_be_100(self):
         """Language proficiency percentages must sum to 100."""
-        packet = ivp_packet.Packet()
         record = make_filled_record()
         record['eng_percentage_english'] = '20'
         record['eng_percentage_spanish'] = '9001'
 
+        ipacket = packet.Packet()
         with self.assertRaises(Exception):
-            builder.add_cls(record, packet)
+            clsform.add_cls(record, ipacket, ivp_forms)
+
+        fpacket = packet.Packet()
+        with self.assertRaises(Exception):
+            clsform.add_cls(record, fpacket, fvp_forms)
 
     def test_check_cls_date(self):
         """
         Having a CLS with a visit date before June 1, 2017 raises an exception.
         """
-        packet = ivp_packet.Packet()
         record = make_filled_record()
         record['visityr'] = '2016'
 
+        ipacket = packet.Packet()
         with self.assertRaises(Exception):
-            builder.add_cls(record, packet)
+            clsform.add_cls(record, ipacket, ivp_forms)
+
+        fpacket = packet.Packet()
+        with self.assertRaises(Exception):
+            clsform.add_cls(record, fpacket, fvp_forms)
 
     def test_cls_form_marked_complete(self):
         """If the completed CLS form is not marked complete, raise."""
-        packet = ivp_packet.Packet()
         record = make_filled_record()
         record['form_cls_linguistic_history_of_subject_complete'] = '0 or 1'
 
+        ipacket = packet.Packet()
         with self.assertRaises(Exception):
-            builder.add_cls(record, packet)
+            clsform.add_cls(record, ipacket, ivp_forms)
+
+        fpacket = packet.Packet()
+        with self.assertRaises(Exception):
+            clsform.add_cls(record, fpacket, fvp_forms)
 
 
 def make_blank_record():
