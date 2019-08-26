c912b3c104d1e11ed646e37c47b57de9110054ce
Taeber Rapczak <taeber@ufl.edu>
2019-08-26 17:21:26 -0400

    Write a non-IO dependent unit test for filters
    See PR #76 for more details: https://github.com/ctsit/nacculator/pull/76

--- tests/filter_test.py
+++ tests/filter_test.py
@@ -1,79 +1,47 @@
 import csv
 import io
-import os
-import tempfile
 import unittest
 
 from nacc.uds3 import filters
 
+
 class TestFilters(unittest.TestCase):
 
-    def test_ptid_not_current_in_NACC(self):
-        ''' Visits already in NACC's Current db should be filtered '''
-        subj_list, subj_list_name = make_subj_list()
-        config, config_name = make_config(subj_list_name)
-        data = make_data_file()
-        output_file = io.BytesIO()
+    def test_filter_clean_ptid_removes_visits_in_nacc_current(self):
+        '''
+        `filter_clean_ptid` should remove visit data that is already in NACC's
+        Current database.
+        '''
 
-        with open(subj_list_name, 'a') as subj_f:
-            sbj_writer = csv.writer(subj_f, delimiter=',')
-            sbj_writer.writerow(['110001', 'I', '001', 'Current'])
-            sbj_writer.writerow(['110002', 'I', '001', 'Working'])
-            sbj_writer.writerow(['110003', 'F', '002', 'Current'])
-            sbj_writer.writerow(['110004', 'F', '002', 'Working'])
+        subjects = '''
+Patient ID,Packet type,Visit Num,Status
+110001,I,001,Current
+110002,I,001,Working
+110003,F,002,Current
+110004,F,002,Working
+'''.strip()
 
-        data_writer = csv.writer(data, delimiter=',', quotechar='"')
-        data_writer.writerow(['110001', 'initial_visit_year_arm_1', '3',
-                              '99', '1', '1', '2019', '001', 'ABC', '2'])
-        data_writer.writerow(['110002', 'initial_visit_year_arm_1', '3',
-                              '99', '1', '1', '2019', '001', 'ABC', '2'])
-        data_writer.writerow(['110003', 'followup_visit_yea_arm_1', '3', 
-                              '99', '1', '1', '2019', '002', 'ABC', '2'])
-        data_writer.writerow(['110004', 'followup_visit_yea_arm_1', '3',
-                              '99', '1', '1', '2019', '002', 'ABC', '2'])
-        data.seek(0)
+        redcap_data = '''
+ptid,redcap_event_name,formver,adcid,visitmo,visitday,visityr,visitnum,initials,header_complete
+110001,initial_visit_year_arm_1,3,99,1,1,2019,001,ABC,2
+110002,initial_visit_year_arm_1,3,99,1,1,2019,001,ABC,2
+110003,followup_visit_yea_arm_1,3,99,1,1,2019,002,ABC,2
+110004,followup_visit_yea_arm_1,3,99,1,1,2019,002,ABC,2
+'''.strip()
 
-        try:
-            filters.filter_clean_ptid(data, config_name, output_file)
-            surviving_ids = []
-            expected_ids = ['110002', '110004']
-            output_file.seek(0)
-            output_reader = csv.DictReader(output_file)
-            for row in output_reader:
-                surviving_ids.append(row['ptid'])
-            self.assertListEqual(surviving_ids, expected_ids)
-        finally:
-            os.close(subj_list)
-            os.remove(subj_list_name)
-            os.close(config)
-            os.remove(config_name)
+        actual = []
+        with io.BytesIO(redcap_data) as data, \
+                io.BytesIO("") as results, \
+                io.BytesIO(subjects) as nacc_packet_file:
 
+            filters.filter_clean_ptid_do(data, nacc_packet_file, results)
 
-def make_subj_list():
-    ''' This csv mimics the one pulled from the NACC website.
-        It shows which visits are in the Current or Working directory.'''
-    subj_list, subj_list_name = tempfile.mkstemp(suffix='.csv')
-    with open(subj_list_name, "w") as f:
-        writer= csv.writer(f, delimiter=',')
-        writer.writerow(['Patient ID', 'Packet type', 'Visit Num', 'Status'])
-    return subj_list, subj_list_name
+            # Reset the file position indicator so DictReader reads from the
+            # beginning of the results "file".
+            results.seek(0)
+            reader = csv.DictReader(results)
+            for row in reader:
+                actual.append(row['ptid'])
 
-
-def make_config(subj_list_name=None):
-    config, config_name = tempfile.mkstemp()
-    with open(config_name, 'w') as conf_f:
-        conf_f.write('[DEFAULT]\n')
-        conf_f.write('\n[filter_clean_ptid]\n')
-        conf_f.write('filepath: ' + subj_list_name + '\n')
-        conf_f.write('\n[filter_remove_ptid]\n')
-        conf_f.write('ptid_format: 11\d.*\n')
-    return config, config_name
-
-
-def make_data_file():
-    data = io.BytesIO()
-    writer = csv.writer(data, delimiter=',', quotechar='"')
-    writer.writerow(['ptid','redcap_event_name','formver',
-                     'adcid','visitmo','visitday','visityr',
-                     'visitnum','initials','header_complete'])
-    return data
+        expected = ['110002', '110004']
+        self.assertListEqual(actual, expected)
