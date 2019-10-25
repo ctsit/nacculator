7a3d5f8b6d91ec998101befe9888ff930aae11e7
Samantha Emerson <s.emerson@ufl.edu>
2019-10-25 15:08:38 -0400

    Add ftld folders to nacc directory with forms and builders for ivp and fvp

---
+++ nacc/ftld/packet.py
@@ -0,0 +1,32 @@
+###############################################################################
+# Copyright 2015-2016 University of Florida. All rights reserved.
+# This file is part of UF CTS-IT's NACCulator project.
+# Use of this source code is governed by the license found in the LICENSE file.
+###############################################################################
+
+
+# Unsure if this search function works on the "builder" file or the "forms" file. 
+
+class Packet(list):
+    """
+    A collection of FTLD Forms
+
+    This class makes it convenient to access a field, which are all uniquely
+    named, regardless of which form they are in.
+    """
+
+    def __init__(self):
+        self._cache = dict()
+
+    def __getitem__(self, key):
+        """
+        Searches through each form in the packet for the field, `key`
+
+        Example:
+            packet['RESTTRL'] is equivalent to:
+            packet.__getitem__('RESTTRL')
+        """
+        if key in self._cache:
+            return self._cache[key]
+
+        raise KeyError(key)
