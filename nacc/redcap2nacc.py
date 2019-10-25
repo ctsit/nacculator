2c951cf1b998d28dca719737ecfef712a3f2cc20
Samantha Emerson <s.emerson@ufl.edu>
2019-10-25 14:28:15 -0400

    Add check_characters function to redcap2nacc and clean up using the linter

--- nacc/redcap2nacc.py
+++ nacc/redcap2nacc.py
@@ -28,7 +28,6 @@ def check_blanks(packet, options):
     """
     Parses rules for when each field should be blank and then checks them
     """
-    pattern = re.compile(r"Blank if Question \d+ (\w+) (ne|=) (\d+)")
     warnings = []
 
     for form in packet:
@@ -55,6 +54,54 @@ def check_blanks(packet, options):
 
     return warnings
 
+
+def check_characters(packet):
+    """
+    Checks fields for any of 4 special characters: & ' " %
+    If these characters are found, throws an error and skips the ptid.
+    """
+    # In the future, might be good to search only "Char" fields if possible.
+    warnings = []
+
+    for form in packet:
+        # Find all fields that have any of the forbidden characters, and
+        #   figure out which characters are present in the string.
+        # If they are found, append an error to our error file and skip the PTID
+        for field in [f for f in form.fields.values()]:
+            chars = ['\'', '\"','&','%%']
+
+            if any((c in chars) for c in field.value):
+                quote = re.search('\'', field.value)
+                dquote = re.search('\"', field.value)
+                amp = re.search('&', field.value)
+                percent = re.search('%', field.value)
+
+                incompatible = []
+                if quote:
+                    quote = "'"
+                    incompatible.append(quote)
+                
+                if dquote:
+                    dquote = '"'
+                    incompatible.append(dquote)
+                
+                if amp:
+                    amp = '&'
+                    incompatible.append(amp)
+                
+                if percent:
+                    percent = '%'
+                    incompatible.append(percent)
+                
+                space = " "
+                character = space.join(incompatible)
+
+                warnings.append(
+                    "\'%s\' is \'%s\', which has invalid character(s) %s . This field can have any text or numbers, but cannot include single quotes \', double quotes \", ampersands & or percentage signs %% " %
+                    (field.name, field.value, character))
+    
+    return warnings
+                
 
 def check_single_select(packet):
     """ Checks the values of sets of interdependent questions
@@ -194,17 +241,27 @@ def convert(fp, options, out=sys.stdout, err=sys.stderr):
             traceback.print_exc()
             continue
 
+        try:
+            warnings += check_characters(packet)
+        except KeyError:
+            print("[SKIP] Error for ptid : " + str(record['ptid']), file=err)
+            traceback.print_exc()
+            continue
+
         if not options.np and not options.m and not options.lbd: 
             warnings += check_single_select(packet)
 
         if warnings:
+            print("[SKIP] Error for ptid : " + str(record['ptid']), file=err)
+            traceback.print_exc()
             print("\n".join(warnings), file=err)
+            continue
 
         for form in packet:
             
             try:
                 print(form, file=out)
-            except AssertionError as e:
+            except AssertionError:
                 print("[SKIP] Error for ptid : " + str(record['ptid']), file=err)
                 traceback.print_exc()
                 continue
