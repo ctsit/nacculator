87f5761f5f67d5a7bec06bd8c01c4495fbfb8c1b
Samantha Emerson <s.emerson@ufl.edu>
2019-11-07 15:51:20 -0500

    Fix unit test

--- tests/invalid_character_checker_test.py
+++ tests/invalid_character_checker_test.py
@@ -20,19 +20,19 @@ class TestInvalidCharacters(unittest.TestCase):
         self.assertTrue(found)
 
     def test_many_characters(self):
-        field = Field('FOTHMUSX', r'ag%fd"fg')
+        field = Field('FOTHMUSX', 'Char', 0, 1, allowable_values='', value='ag%fd"fg')
         found_many = check_for_bad_characters(field)
         many = len(found_many)
         self.assertEqual(many, 2)
 
     def test_closed_double_quotes(self):
-        field = Field('FOTHMUSX', 'ag\"fd\"fg')
+        field = Field('FOTHMUSX', 'Char', 0, 1, allowable_values='', value='ag\"fd\"fg')
         found_two_doublequotes = check_for_bad_characters(field)
         dups = found_two_doublequotes[0]
         self.assertEqual(dups, '" (2)')
 
     def test_closed_single_quotes(self):
-        field = Field('FOTHMUSX', 'ag\'fdf\'g')
+        field = Field('FOTHMUSX', 'Char', 0, 1, allowable_values='', value='ag\'fdf\'g')
         found_two_quotes = check_for_bad_characters(field)
         dups1 = found_two_quotes[0]
         self.assertEqual(dups1, '\' (2)')
