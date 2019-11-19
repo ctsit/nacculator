2a351567f6f7534d59c25913c6d92666027dcaef
Samantha Emerson <s.emerson@ufl.edu>
2019-11-19 10:48:54 -0500

    Adjust unit tests to account for inclusive_range rules and changed z1 headers

--- tests/check_within_range_test.py
+++ tests/check_within_range_test.py
@@ -1,17 +1,6 @@
 import unittest
 
 import nacc.uds3
-from nacc.uds3 import Field
-from nacc import redcap2nacc
-from nacc.uds3 import packet
-
-
-class option():
-    iorf = 'ivp'
-    lbd = False
-    ftld = False
-    ivp = True
-    fvp = False
 
 
 class TestWithinRange(unittest.TestCase):
@@ -22,45 +11,25 @@ class TestWithinRange(unittest.TestCase):
     "allowable_values" and "inclusive_range" variables.
     '''
 
-    def setUp(self):
-        self.options = option()
-
-    def test_for_Field_variables_entered(self):
-        ''' Have it check that the Field class is picking up our input '''
-        record = make_filled_form()
-        record['reason'] = '1'
-        ipacket = make_builder(record)
-        item = Form.field.REASON
-
-        result = Field(item)
-        expected = []  # The expected output will be different... no output?
-        self.assertEqual(result, expected)
-
     def test_for_Char_fields_skipping_range_check(self):
         '''
         Have it make sure that only Num fields are being checked for range
         '''
-        record = make_filled_form()
-        record['racex'] = 'race'
-        ipacket = make_builder(record)
-        item = Form.field.RACEX
+        racex = nacc.uds3.Field(name='RACEX', typename='Char', position=(136, 195), length=60, inclusive_range=None, allowable_values=[], blanks=[])
 
-        result = Field(item)
-        expected = []
-        self.assertEqual(result, expected)
+        racex.value = 'race'
+        expected = 'race'
+        self.assertEqual(racex.val, expected)
 
     def test_for_allowed_values_within_range(self):
         '''
         Ensure that the 'if self.allowable_values' statement returns expected
         errors
         '''
-        record = make_filled_form()
-        record['reason'] = '1'
-        ipacket = make_builder(record)
-        item = Form.field.REASON
+        reason = nacc.uds3.Field(name='REASON', typename='Num', position=(45, 45), length=1, inclusive_range=(1, 4), allowable_values=['4', '2', '1', '9'], blanks=[])
 
         with self.assertRaises(ValueError):
-            result = Field(item)
+            reason.value = '10'
 
     def test_for_allowed_values_out_of_range(self):
         '''
@@ -69,27 +38,18 @@ class TestWithinRange(unittest.TestCase):
         allowable_values, such as a field that expects 1-3, but also allows 9
         (like 9 = Unknown)
         '''
-        record = make_filled_form()
-        record['reason'] = '9'
-        ipacket = make_builder(record)
-        item = Form.field.REASON
+        reason = nacc.uds3.Field(name='REASON', typename='Num', position=(45, 45), length=1, inclusive_range=(1, 4), allowable_values=['4', '2', '1', '9'], blanks=[])
 
-        result = Field(item)
-        expected = []
-        self.assertEqual(item, expected)
+        reason.value = '9'
+        expected = '9'
+        self.assertEqual(reason.val, expected)
 
-    def test_for_forbidden_values_within_range(self):
-        '''
-        Ensure that values that are within the allowed range, but not part
-        of allowable_values, return an error
-        '''
-        record = make_filled_form()
-        record['reason'] = '3'
-        ipacket = make_builder(record)
-        item = Form.field.REASON
-
-        with self.assertRaises(ValueError):
-            result = Field(item)
+    # value.setter cannot catch value errors where the value is technically
+    # within the inclusive range, but not one of the allowable values (for
+    # example, 'REASON' above has the range 1-4, but only allowable values of
+    # 1, 2, and 4) because other fields have that option and SHOULDN'T return
+    # errors (for example, a field that has the inclusive_range '1-12' but
+    # also a survey error code '88' as the only entry in its allowable_values)
 
     def test_for_within_range_without_allowable_values(self):
         '''
@@ -97,54 +57,10 @@ class TestWithinRange(unittest.TestCase):
         out-of-range errors when there is an inclusive_range but no specific
         allowable_values
         '''
-        record = make_filled_form()
-        record['kids'] = '16'
-        ipacket = make_builder(record)
-        item = Form.field.KIDS
+        kids = nacc.uds3.Field(name='KIDS', typename='Num', position=(956, 957), length=2, inclusive_range=(0, 15), allowable_values=[], blanks=[])
 
         with self.assertRaises(ValueError):
-            result = Field(item)
-
-
-def make_builder(record: dict) -> packet.Packet:
-    ipacket = packet.Packet()
-    form = Form()
-    form.RACEX = record['racex']
-    form.REASON = record['reason']
-    form.KIDS = record['kids']
-    ipacket.append(form)
-
-    update_header(record, ipacket)
-
-    return ipacket
-
-
-def update_header(record: dict, packet: packet.Packet):
-    for header in packet:
-        header.PTID = record['ptid']
-
-
-def make_filled_form() -> dict:
-    return {
-        'ptid': '1',
-        'racex': '',
-        'reason': '',
-        'kids': '',
-    }
-
-
-def header_fields():
-    fields = {}
-    fields['PTID'] = nacc.uds3.Field(name='PTID', typename='Char', position=(1, 10), length=10, inclusive_range=None, allowable_values=[], blanks=[])
-    return fields
-
-
-class Form(nacc.uds3.FieldBag):
-    def __init__(self):
-        self.fields = header_fields()
-        self.fields['RACEX'] = nacc.uds3.Field(name='RACEX', typename='Char', position=(136, 195), length=60, inclusive_range=None, allowable_values=[], blanks=[])
-        self.fields['REASON'] = nacc.uds3.Field(name='REASON', typename='Num', position=(45, 45), length=1, inclusive_range=(1, 4), allowable_values=['4', '2', '1', '9'], blanks=[])
-        self.fields['KIDS'] = nacc.uds3.Field(name='KIDS', typename='Num', position=(956, 957), length=2, inclusive_range=(0, 15), allowable_values=[], blanks=[])
+            kids.value = '16'
 
 
 if __name__ == "__main__":
