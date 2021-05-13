07efe32f3a59d1e0f275005d439e8b3dfcebc965
Samantha Emerson <s.emerson@ufl.edu>
2021-05-13 15:42:12 -0400

    Add LBD event detection logic to FVP events as well

--- nacc/redcap2nacc.py
+++ nacc/redcap2nacc.py
@@ -173,7 +173,10 @@ def check_redcap_event(options, record) -> bool:
             return False
     elif options.lbd and options.fvp:
         event_name = 'follow'
-        form_match_lbd = record['lbd_fvp_b1l_complete']
+        try:
+            form_match_lbd = record['lbd_fvp_b1l_complete']
+        except KeyError:
+            form_match_lbd = record['lbd_fvp_b1l_clinical_symptoms_and_exam_complete']
         if form_match_lbd in ['0', '']:
             return False
     elif options.lbdsv and options.ivp:
@@ -186,7 +189,10 @@ def check_redcap_event(options, record) -> bool:
             return False
     elif options.lbdsv and options.fvp:
         event_name = 'follow'
-        form_match_lbd = record['lbd_fvp_b1l_complete']
+        try:
+            form_match_lbd = record['lbd_fvp_b1l_complete']
+        except KeyError:
+            form_match_lbd = record['lbd_fvp_b1l_clinical_symptoms_and_exam_complete']
         if form_match_lbd in ['0', '']:
             return False
     elif options.ftld and options.ivp:
