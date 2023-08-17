ce4d659b29ba6801d080d6b33c4e5a79d84dea6e
Kshitij Sinha <kshitij.sinha1608@gmail.com>
2023-08-17 17:52:13 -0400

    Updated configure_logging

--- nacc/logger.py
+++ nacc/logger.py
@@ -2,15 +2,12 @@ import logging
 import sys
 from report_handler.report_handler import ReportHandler
 
-global report_handler
 
-
-def configure_logging(config):
+def configure_logging(config, handlers: list[logging.Handlers] = []):
     fmt = '%(asctime)s  %(levelname)-9s  %(message)s'
 
     logging.basicConfig(level=logging.DEBUG, format=fmt,
                         filename=ReportHandler.prevent_overwrite(config.log))
-
     if config.quiet:
         # Do not write logs to console; normal output still goes to stdout.
         return
@@ -24,36 +21,10 @@ def configure_logging(config):
     else:
         console.setLevel(logging.INFO)
 
-    global report_handler
-    report_handler = ReportHandler()
-
     # Gets the root logger and any config changes here affects logging across the code base
     logger = logging.getLogger()
 
     logger.addHandler(console)
-    logger.addHandler(report_handler)
 
-
-if __name__ == "logger":
-    configure_logging()
-
-# from python_db_logger.db_logger import DBLogger
-# from python_db_logger.connection_helper import ConnectionHelper
-# import logging
-
-# """
-# Why a separate file? Since Python imports provide the same 
-# reference of a module wherever imported, it is easier to create
-# DBLogger in a separate file and import it everywhere. This way,
-# we don't have to worry about creating and passing the dbLogger 
-# instances from one file to another.
-# """
-
-# logging_instance = logging.getLogger('nacculator-db-logger')
-# logging_instance.setLevel(logging.DEBUG)
-
-# db_logger: DBLogger = DBLogger(
-#     logging_instance,
-#     ConnectionHelper(dot_env_file='.env').connect_to_mysql(),
-#     write_to_prod=True
-# )
+    for handler in handlers:
+        logger.addHandler(handler)
