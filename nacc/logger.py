6252ad8149549743ef9a354f335f78585e05eadb
Kshitij Sinha <kshitij.sinha1608@gmail.com>
2023-11-01 11:01:18 -0400

    Removed commented code

--- nacc/logger.py
+++ nacc/logger.py
@@ -8,19 +8,11 @@ def configure_logging(config, handlers: list[logging.Handler] = []):
 
     logging.basicConfig(level=logging.DEBUG, format=fmt,
                         filename="logs.log")
-    # if config.quiet:
-    #     # Do not write logs to console; normal output still goes to stdout.
-    #     return
 
     console = logging.StreamHandler(sys.stderr)
     formatter = logging.Formatter(fmt)
     console.setFormatter(formatter)
     console.setLevel(logging.DEBUG)
-    # if config.verbose:
-    #     # Increase verbosity if the "--verbose" flag was specified.
-    #     console.setLevel(logging.DEBUG)
-    # else:
-    #     console.setLevel(logging.INFO)
 
     # Gets the root logger and any config changes here affects logging across the code base
     logger = logging.getLogger()
